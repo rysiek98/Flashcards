871e86e48709292d9b8577fae2022a94cd50236c
rysiek98 <52510889+rysiek98@users.noreply.github.com>
2020-08-20 16:47:12 +0200

    add PUT & DELETE method, run PSQL

--- src/main/java/com/flashcardsApplication/flashcards/FlashcardsService.java
+++ src/main/java/com/flashcardsApplication/flashcards/FlashcardsService.java
@@ -1,20 +1,45 @@
 package com.flashcardsApplication.flashcards;
 
+import com.flashcardsApplication.card.CardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.server.ResponseStatusException;
+import javax.transaction.Transactional;
+import java.util.Comparator;
 import java.util.List;
+import static com.flashcardsApplication.card.CardService.updateCard;
 
 @Service
 @RequiredArgsConstructor
 public class FlashcardsService {
+
     private final FlashcardsRepository flashcardsRepository;
 
     List<Flashcards> findAllFlashcards(){
-        return flashcardsRepository.findAll();
+        List<Flashcards> flashcards = flashcardsRepository.findAll();
+        flashcards.sort(Comparator.comparing(f->f.getId()));
+        return flashcards;
     }
 
     public Flashcards add(Flashcards flashcards) {
         return flashcardsRepository.save(flashcards);
+    }
+
+    public Flashcards findById(long id) {
+        return flashcardsRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    public String deleteById(long id) {
+        flashcardsRepository.delete(findById(id));
+        return "DELETED";
+    }
+
+    @Transactional
+    public Flashcards updateById(Flashcards flashcards) {
+        Flashcards updateFlashcards = findById(flashcards.getId());
+        updateCard(updateFlashcards.getCards(),flashcards.getCards());
+        return flashcardsRepository.save(updateFlashcards);
     }
 }
